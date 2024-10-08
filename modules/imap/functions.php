c9e5fefd6d1f7acb1e76940c2de38f53bf80c4c0
Baraka Kinywa <bkinywa24@gmail.com>
2024-10-08 02:22:06 +0200

    [FIX]Imap quota: Fixing quota appears only once folders are expanded

--- modules/imap/functions.php
+++ modules/imap/functions.php
@@ -181,7 +181,6 @@ function format_imap_folder_section($folders, $id, $output_mod, $with_input = fa
         $results .= '<li class="quota_info"><div class="progress bg-secondary border"><div class="progress-bar bg-light" style="width:'.$quota.'%"></div></div>'.$quota.'% used on '.$quota_max.' MB</li>';
     }
 
-    $results .= '</ul>';
     return $results;
 }}
 
